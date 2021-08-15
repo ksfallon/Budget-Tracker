cdca430157711454e190a96454e5510b6ec08ae2
ksfallon <fallon.katelin@gmail.com>
2021-08-15 11:54:11 -0400

    updating db.js to get indexedDB working

--- public/db.js
+++ public/db.js
@@ -1,3 +1,9 @@
+const indexedDB =
+  window.indexedDB ||
+  window.mozIndexedDB ||
+  window.webkitIndexedDB ||
+  window.msIndexedDB ||
+  window.shimIndexedDB;
 //indexedDB HERE
 let db;
 // let budgetVersion
@@ -8,13 +14,13 @@ request.onupgradeneeded = ({target}) => {
   // create object store inside of our indexed.DB
     db = target.result
     // This creates an object store alled 'BudgetStorage'
-    db.createObjectStore("BudgetNew", 
-    {keypath: "listID", auto_increment:true}); //keypath can be used to query on and we want auto_increment to be TRUE
+    db.createObjectStore("pendingBudget", 
+    {auto_increment:true}); //keypath can be used to query on and we want auto_increment to be TRUE
 
 };
 
-request.onsuccess = (event) => {
-db = event.target.result;
+request.onsuccess = ({target}) => {
+db = target.result;
 
 //need to make sure to have function called CHECK_DATABASE which will send data to backend once online
 if (navigator.onLine) {
@@ -23,20 +29,21 @@ if (navigator.onLine) {
 };
 
 request.onerror = function (event) {
-    console.log("You have an error", event);
+    console.log(`You have an error: ${event.target.errorCode}`);
 };
 
 const saveRecord = (record) => {
   // create a transaction on the pending db with readwrite access. access your pending object store. add record to your store with add method.
-  console.log("is anything going through? ", record);
-  const transaction = db.transaction(["BudgetNew"], "readwrite");
-  const BudgetStore = transaction.objectStore("BudgetNew");
-  console.log("is anything going through still? ", BudgetStore);
-  BudgetStore.add(record).then( (data) => {
-    console.log("DATA", data)
-  }).catch((error) => {
-    console.log("ERR", error)
-  })
+  // console.log("is anything going through? ", record);
+  const transaction = db.transaction(["pendingBudget"], "readwrite");
+  const budgetStore = transaction.objectStore("pendingBudget");
+  // console.log("is anything going through still? ", BudgetStore);
+  budgetStore.add(record)
+  // .then( (data) => {
+  //   console.log("DATA", data)
+  // }).catch((error) => {
+  //   console.log("ERR", error)
+  // })
 }
 
 const checkDatabase = () => {
