6319b478be7641c4e5b209a385a68bc091c82760
ksfallon <fallon.katelin@gmail.com>
2021-08-15 13:59:54 -0400

    updated service worker and db.js to see if i could get offline to work

--- public/db.js
+++ public/db.js
@@ -4,76 +4,62 @@ const indexedDB =
   window.webkitIndexedDB ||
   window.msIndexedDB ||
   window.shimIndexedDB;
-//indexedDB HERE
+​
 let db;
-// let budgetVersion
-// create a new db request for a "budget" database. its version 1 in our indexedDB
-const request = window.indexedDB.open("budget", 1) // const request = window.indexedDB.open("budget", budgetVersion || 21);
-
-request.onupgradeneeded = ({target}) => {
-  // create object store inside of our indexed.DB
-    db = target.result
-    // This creates an object store alled 'BudgetStorage'
-    db.createObjectStore("pendingBudget", 
-    {auto_increment:true}); //keypath can be used to query on and we want auto_increment to be TRUE
-
+const request = indexedDB.open('budget', 1);
+​
+request.onupgradeneeded = ({ target }) => {
+  const db = target.result;
+  db.createObjectStore('pending', { autoIncrement: true });
 };
-
-request.onsuccess = ({target}) => {
-db = target.result;
-
-//need to make sure to have function called CHECK_DATABASE which will send data to backend once online
-if (navigator.onLine) {
+​
+request.onsuccess = ({ target }) => {
+  db = target.result;
+​
+  // check if app is online before reading from db
+  if (navigator.onLine) {
     checkDatabase();
-    }
+  }
 };
-
+​
 request.onerror = function (event) {
-    console.log(`You have an error: ${event.target.errorCode}`);
+  console.log(`Woops! ${event.target.errorCode}`);
 };
-
-const saveRecord = (record) => {
-  // create a transaction on the pending db with readwrite access. access your pending object store. add record to your store with add method.
-  // console.log("is anything going through? ", record);
-  const transaction = db.transaction(["pendingBudget"], "readwrite");
-  const budgetStore = transaction.objectStore("pendingBudget");
-  // console.log("is anything going through still? ", BudgetStore);
-  budgetStore.add(record)
-  // .then( (data) => {
-  //   console.log("DATA", data)
-  // }).catch((error) => {
-  //   console.log("ERR", error)
-  // })
+​
+function saveRecord(record) {
+  const transaction = db.transaction(['pending'], 'readwrite');
+  const store = transaction.objectStore('pending');
+​
+  store.add(record);
 }
-
-const checkDatabase = () => {
-  // open a transaction on your pending db, access your pending object store and get all records from store and set to a variable
-  const transaction = db.transaction(["pendingBudget"], "readwrite");
-  const budgetStore = transaction.objectStore("pendingBudget");
-  const getAll = budgetStore.getAll
-
-    getAll.onsuccess = function () {
-        if (getAll.result.length > 0) {
-            fetch('/api/transaction/bulk', {
-                method: 'POST',
-                body: JSON.stringify(getAll.result),
-                headers: {
-                    Accept: 'application/json, text/plain, */*',
-                    'Content-Type': 'application/json',
-                },
-        })
+​
+function checkDatabase() {
+  const transaction = db.transaction(['pending'], 'readwrite');
+  const store = transaction.objectStore('pending');
+  const getAll = store.getAll();
+​
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction/bulk', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
+        },
+      })
         .then((response) => {
-          return response.json()
+          return response.json();
         })
         .then(() => {
-          // if successful, open a transaction on your pending db
-        const transaction = db.transaction(["pendingBudget"], "readwrite");
-        const budgetStore = transaction.objectStore("pendingBudget");
-        store.clear();
+          // delete records if successful
+          const transaction = db.transaction(['pending'], 'readwrite');
+          const store = transaction.objectStore('pending');
+          store.clear();
         });
-        }
-    };
+    }
+  };
 }
-
+​
 // listen for app coming back online
 window.addEventListener('online', checkDatabase);
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,71 +1,66 @@
-const STATIC_CACHE = "my-site-cache-v1"; // holds all of our webpages that will render
-const DATA_CACHE_TIME = "data-cache-v1"; // hold all of the data thats entered - any changes to the Database goes inside here
-
-const FILES_TO_CACHE = [   //const to cache are all the ones under PUBLIC
-  "/",
-  "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png",
-  "/index.html",
-  "/index.js",
-  "/style.css",
-  "/db.js",
-  "/manifest.webmanifest",
+const CACHE_NAME = 'my-site-cache-v1';
+const DATA_CACHE_NAME = 'data-cache-v1';
+​
+const urlsToCache = [
+  '/',
+  '/db.js',
+  '/index.js',
+  '/manifest.json',
+  '/styles.css',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
 ];
-
-// self points straight to service worker - when you add an event listener when its offline THEN, take all the files needed for statics use and activate them.
-self.addEventListener("install", (event) => {
- //Perform install steps
+​
+self.addEventListener('install', function (event) {
+  // Perform install steps
   event.waitUntil(
-    caches
-      .open(STATIC_CACHE) //first get the static-cache - (line 11 for def)
-      .then((cache) => {
-        return cache.addAll(FILES_TO_CACHE) //second get all of the files - homepage link (/), icons, index, style, database(db);
-        .catch(err => console.log('ERROR: ', err));
-      })
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache).catch(err => console.log('err!!!!!! ', err));
+    })
   );
-  self.skipWaiting();
 });
-
-self.addEventListener("fetch", (event) => {
-  // cache all GET requests to/api routes
-    if (event.request.url.includes("/api/")) {
-      event.respondWith(
-      // make network request and fallback to cache if network request fails (offline)
-        caches
-          .open(DATA_CACHE_TIME) // .. we will cache that data as the response
-            .then((cache) => {
-              return fetch(event.request)
-                .then((response) => {
-                  // If the response is good, clone it and store it in the cache
-                  if (response.status === 200) {
-                    cache.put(event.request.url, response.clone()); //saying not supported
-                  }
-                  return response;
-                })
-                .catch((err) => {
-                  return caches.match(event.request)
-                });
-            })
-            .catch(error => console.log(error))  
-        );
-        return;
-    }
-
-  // use cache first for all other requests for performance
+​
+self.addEventListener('fetch', function (event) {
+  // cache all get requests to /api routes
+  if (event.request.url.includes('/api/')) {
     event.respondWith(
-      fetch(event.request).catch(function () {
-        return caches.match(event.request)
-        .then(function (response) {
-          if (response) {
-            return response;
-          }
-          if (event.request.headers.get('accept').includes('text/html')) {
-                      // return the cached home page for all requests for html pages
-            return caches.match('/')
-          }
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(event.request)
+            .then((response) => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
+​
+              return response;
+            })
+            .catch((err) => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(event.request);
+            });
         })
-      })
-    )
+        .catch((err) => console.log(err))
+    );
+​
+    return;
+  }
+​
+  event.respondWith(
+    fetch(event.request).catch(function () {
+      return caches.match(event.request).then(function (response) {
+        if (response) {
+          return response;
+        }
+        if (event.request.headers.get('accept').includes('text/html')) {
+          // return the cached home page for all requests for html pages
+          return caches.match('/');
+        }
+      });
+    })
+  );
 });
 
 // self.addEventListener("activate", (event) => {
