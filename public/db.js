3967ec282e64530d5e9bb75c805a187adc8b20ab
ksfallon <fallon.katelin@gmail.com>
2021-08-16 10:11:58 -0400

    changing up service worker and db.js again to see why my service worker isn't connecting

--- public/db.js
+++ public/db.js
@@ -22,23 +22,23 @@ request.onsuccess = ({ target }) => {
   }
 };
 ​
-request.onerror = ({target}) => {
-  console.log(`Woops! ${target.errorCode}`);
+request.onerror = function (event) {
+  console.log(`Woops! ${event.target.errorCode}`);
 };
 ​
-const saveRecord = (record) => {
-  const transaction = db.transaction(["pending"], "readwrite");
-  const store = transaction.objectStore("pending");
+function saveRecord(record) {
+  const transaction = db.transaction(['pending'], 'readwrite');
+  const store = transaction.objectStore('pending');
 ​
   store.add(record);
 }
 ​
-const checkDatabase = () => {
-  const transaction = db.transaction(["pending"], "readwrite");
-  const store = transaction.objectStore("pending");
-  const getAll = store.getAll(); // to grab 
+function checkDatabase() {
+  const transaction = db.transaction(['pending'], 'readwrite');
+  const store = transaction.objectStore('pending');
+  const getAll = store.getAll();
 ​
-  getAll.onsuccess = () => {
+  getAll.onsuccess = function () {
     if (getAll.result.length > 0) {
       fetch('/api/transaction/bulk', {
         method: 'POST',
@@ -48,21 +48,88 @@ const checkDatabase = () => {
           'Content-Type': 'application/json',
         },
       })
-        .then(response => response.json())
+        .then((response) => {
+          return response.json();
+        })
         .then(() => {
           // delete records if successful
-          const transaction = db.transaction(["pending"], "readwrite");
-          const store = transaction.objectStore("pending");
+          const transaction = db.transaction(['pending'], 'readwrite');
+          const store = transaction.objectStore('pending');
           store.clear();
-          console.log("If i appear, offline data was moved to Mongo Atlas DB")
-        })
-        .catch((err) => {console.error("Error when grabbing offline data when back online", err)});
+        });
     }
   };
 }
 ​
 // listen for app coming back online
 window.addEventListener('online', checkDatabase);
+
+
+// const indexedDB =
+//   window.indexedDB ||
+//   window.mozIndexedDB ||
+//   window.webkitIndexedDB ||
+//   window.msIndexedDB ||
+//   window.shimIndexedDB;
+// ​
+// let db;
+// const request = indexedDB.open('budget', 1);
+// ​
+// request.onupgradeneeded = ({ target }) => {
+//   const db = target.result;
+//   db.createObjectStore('pending', { autoIncrement: true });
+// };
+// ​
+// request.onsuccess = ({ target }) => {
+//   db = target.result;
+// ​
+//   // check if app is online before reading from db
+//   if (navigator.onLine) {
+//     checkDatabase();
+//   }
+// };
+// ​
+// request.onerror = ({target}) => {
+//   console.log(`Woops! ${target.errorCode}`);
+// };
+// ​
+// const saveRecord = (record) => {
+//   const transaction = db.transaction(["pending"], "readwrite");
+//   const store = transaction.objectStore("pending");
+// ​
+//   store.add(record);
+// }
+// ​
+// const checkDatabase = () => {
+//   const transaction = db.transaction(["pending"], "readwrite");
+//   const store = transaction.objectStore("pending");
+//   const getAll = store.getAll(); // to grab 
+// ​
+//   getAll.onsuccess = () => {
+//     if (getAll.result.length > 0) {
+//       fetch('/api/transaction/bulk', {
+//         method: 'POST',
+//         body: JSON.stringify(getAll.result),
+//         headers: {
+//           Accept: 'application/json, text/plain, */*',
+//           'Content-Type': 'application/json',
+//         },
+//       })
+//         .then(response => response.json())
+//         .then(() => {
+//           // delete records if successful
+//           const transaction = db.transaction(["pending"], "readwrite");
+//           const store = transaction.objectStore("pending");
+//           store.clear();
+//           console.log("If i appear, offline data was moved to Mongo Atlas DB")
+//         })
+//         .catch((err) => {console.error("Error when grabbing offline data when back online", err)});
+//     }
+//   };
+// }
+// ​
+// // listen for app coming back online
+// window.addEventListener('online', checkDatabase);
 
 //ADDED BACK (8/15/2021 @ 2:30)
 // - keyPath: "listID" on line 13
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,49 +1,27 @@
-const CACHE_NAME = 'static-cache-v1'; //had v2, changed to v1
+const CACHE_NAME = 'my-site-cache-v1';
 const DATA_CACHE_NAME = 'data-cache-v1';
+​
 const urlsToCache = [
   '/',
   '/db.js',
-  '/index.html',
   '/index.js',
   '/manifest.json',
-  '/service-worker.js', //added, maybe its the problem
   '/styles.css',
   '/icons/icon-192x192.png',
   '/icons/icon-512x512.png',
 ];
 ​
-self.addEventListener('install', (event) => {
+self.addEventListener('install', function (event) {
   // Perform install steps
-  // should i add an event.waitUntil for the DATA_CACHE_NAME ?
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('Successfully Opened Cache');
-      return cache.addAll(urlsToCache)
-      .catch(err => console.log('There is an error! ', err));
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache).catch(err => console.log('err!!!!!! ', err));
     })
   );
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", (event) => {
-  // const currentCaches = [STATIC_CACHE, DATA_CACHE_TIME]
-  event.waitUntil(
-      caches.keys().then(cacheData => {
-          return Promise.all(
-            cacheData.map(data => {
-              if (data !== DATA_CACHE_NAME && data !== CACHE_NAME) {
-                //map through to delete old data
-                console.log("old cached data is REMOVED", data)
-                return caches.delete(data)
-              }
-            })
-        );
-    })
-);
-self.clients.claim();
 });
 ​
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', function (event) {
   // cache all get requests to /api routes
   if (event.request.url.includes('/api/')) {
     event.respondWith(
@@ -64,15 +42,15 @@ self.addEventListener('fetch', (event) => {
               return cache.match(event.request);
             });
         })
-        .catch((err) => console.log("Error from fetch function", err))
+        .catch((err) => console.log(err))
     );
 ​
     return;
   }
 ​
   event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request).then((response) => {
+    fetch(event.request).catch(function () {
+      return caches.match(event.request).then(function (response) {
         if (response) {
           return response;
         }
@@ -84,6 +62,93 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
+
+// const CACHE_NAME = 'static-cache-v1'; //had v2, changed to v1
+// const DATA_CACHE_NAME = 'data-cache-v1';
+// const urlsToCache = [
+//   '/',
+//   '/db.js',
+//   '/index.html',
+//   '/index.js',
+//   '/manifest.json',
+//   '/service-worker.js', //added, maybe its the problem
+//   '/styles.css',
+//   '/icons/icon-192x192.png',
+//   '/icons/icon-512x512.png',
+// ];
+// ​
+// self.addEventListener('install', (event) => {
+//   // Perform install steps
+//   // should i add an event.waitUntil for the DATA_CACHE_NAME ?
+//   event.waitUntil(
+//     caches.open(CACHE_NAME).then((cache) => {
+//       console.log('Successfully Opened Cache');
+//       return cache.addAll(urlsToCache)
+//       .catch(err => console.log('There is an error! ', err));
+//     })
+//   );
+//   self.skipWaiting();
+// });
+
+// self.addEventListener("activate", (event) => {
+//   // const currentCaches = [STATIC_CACHE, DATA_CACHE_TIME]
+//   event.waitUntil(
+//       caches.keys().then(cacheData => {
+//           return Promise.all(
+//             cacheData.map(data => {
+//               if (data !== DATA_CACHE_NAME && data !== CACHE_NAME) {
+//                 //map through to delete old data
+//                 console.log("old cached data is REMOVED", data)
+//                 return caches.delete(data)
+//               }
+//             })
+//         );
+//     })
+// );
+// self.clients.claim();
+// });
+// ​
+// self.addEventListener('fetch', (event) => {
+//   // cache all get requests to /api routes
+//   if (event.request.url.includes('/api/')) {
+//     event.respondWith(
+//       caches
+//         .open(DATA_CACHE_NAME)
+//         .then((cache) => {
+//           return fetch(event.request)
+//             .then((response) => {
+//               // If the response was good, clone it and store it in the cache.
+//               if (response.status === 200) {
+//                 cache.put(event.request.url, response.clone());
+//               }
+// ​
+//               return response;
+//             })
+//             .catch((err) => {
+//               // Network request failed, try to get it from the cache.
+//               return cache.match(event.request);
+//             });
+//         })
+//         .catch((err) => console.log("Error from fetch function", err))
+//     );
+// ​
+//     return;
+//   }
+// ​
+//   event.respondWith(
+//     fetch(event.request).catch(() => {
+//       return caches.match(event.request).then((response) => {
+//         if (response) {
+//           return response;
+//         }
+//         if (event.request.headers.get('accept').includes('text/html')) {
+//           // return the cached home page for all requests for html pages
+//           return caches.match('/');
+//         }
+//       });
+//     })
+//   );
+// });
 
 //changed
 // line 1 - 'static-cache-v2'
